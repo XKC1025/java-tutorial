2be575a96fb0618b9e1dda8b87995f0afd8b47ae
XKC1025 <898044807@qq.com>
2019-03-17 16:55:01 +0800

    xml

--- web/src/main/java/com/xkc/learn/jdbc_template/MyJdbcTemplate.java
+++ web/src/main/java/com/xkc/learn/jdbc_template/MyJdbcTemplate.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
---
+++ web/src/main/java/com/xkc/learn/xml/ParseXml.java
@@ -0,0 +1,11 @@
+package com.xkc.learn.xml;
+
+/**
+ * 解析XML文件
+ */
+public class ParseXml {
+    public static void main(String[] args) {
+        // 获取document对象
+
+    }
+}
