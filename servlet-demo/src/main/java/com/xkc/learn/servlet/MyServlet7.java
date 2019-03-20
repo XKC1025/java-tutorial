bb1296fc934db9d7f60a6108b628a2a04bded634
XKC1025 <898044807@qq.com>
2019-03-20 21:28:08 +0800

    modify pom.xml

--- servlet-demo/src/main/java/com/xkc/learn/servlet/MyServlet7.java
+++ servlet-demo/src/main/java/com/xkc/learn/servlet/MyServlet7.java
@@ -1,14 +1,11 @@
 package com.xkc.learn.servlet;
 
-import org.apache.commons.beanutils.BeanUtils;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 /**
@@ -18,6 +15,11 @@ import java.util.Map;
 public class MyServlet7 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 动态获取虚拟目录
+        String contextPath = req.getContextPath();
+
+        resp.sendRedirect("/demo6");
+
         req.setCharacterEncoding("utf-8");
 
         Map<String, String[]> parameterMap = req.getParameterMap();
@@ -26,12 +28,12 @@ public class MyServlet7 extends HttpServlet {
         LoginUser loginUser = new LoginUser();
 
         // BeanUtils给对象赋值
-        try {
-            BeanUtils.populate(loginUser, parameterMap);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            BeanUtils.populate(loginUser, parameterMap);
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
     }
 }
---
+++ servlet-demo/src/main/java/com/xkc/learn/servlet/MyServlet8.java
@@ -0,0 +1,28 @@
+package com.xkc.learn.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@WebServlet("/demo8")
+public class MyServlet8 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 防止中文乱码
+        // resp.setCharacterEncoding("utf-8");
+
+        // 告诉浏览器响应的编码, 防止乱码
+        resp.setContentType("text/html;charset=utf-8");
+
+//        ServletOutputStream outputStream = resp.getOutputStream();
+        PrintWriter writer = resp.getWriter();
+
+        writer.write("Hello, World!你好");
+    }
+}
